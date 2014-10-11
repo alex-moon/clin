c32b01b5a7e8a4482d3e8d996d51177c08afd696
Alex Moon <alex.moon@tangentlabs.co.uk>
2014-10-11 20:00:26 +0100

    Backbone + perfunctory view with demo of how templates work

--- www/static/js/clin.js
+++ www/static/js/clin.js
@@ -1,35 +1,7 @@
 function Clin() {
-    var subscribers = {}
-
-    function subscribe(key, obj) {
-        if (! subscribers.hasOwnProperty(key)) {
-            subscibers[key] = [obj];
-        } else {
-            subscribers[key].push(obj);
-        }
-    }
-
-    function publish(key, data) {
-        if (subscribers.hasOwnProperty(key)) {
-            for (i = 0; i < subscribers[key].length; i++) {
-                subscribers[key][i].tell(key, data);
-            }
-        }
-    }
-
-    function TestView() {
-        var template = '#test-view-template';
-        function tell(key, data) {
-            switch (key) {
-                default:
-                    console.log('TestView.tell', key, data);
-                    break;
-            }
-        }
-        function render(data) {
-            console.log('TestView.render', data);
-        }
-    }
+    var TestView = Backbone.View.extend({
+        template: _.template($('#test-view-template').text())
+    });
 
     return {
         'test_view': new TestView()
