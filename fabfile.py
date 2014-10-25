e0fe0ff17bf3bdf1737e256acfc375b0179973af
Alex Moon <alex.moon@tangentlabs.co.uk>
2014-10-25 22:15:43 +0100

    Some cleanup :)

--- fabfile.py
+++ fabfile.py
@@ -23,7 +23,6 @@ def deploy(tag=None):
     upload()
     unpack()
     manage('syncdb', 'migrate', 'collectstatic --noinput')
-    ensure_log_folder()
     
     uwsgi()
     supervisor()
@@ -31,17 +30,17 @@ def deploy(tag=None):
     
 
 def backup_target_database():
-    result = 'result'
-    while result.lower() not in ['y', 'n', None]:
+    for i in range(0, 3):
         result = prompt('Do you want to backup the database? [y/N]', default='n')
-        if result == 'y':
+        if result.lower() == 'y':
             backup_db()
 
-
-def ensure_log_folder():
-    sudo('mkdir -p /var/log/clin/')
-    sudo('chown -R root:www-data /var/log/clin/')
-    sudo('chmod -R 774 /var/log/clin/')
+        if result.lower() not in ['y', 'n', None]:
+            print "Let's try this again..."
+        else:
+            return
+    print red("Didn't get a sane answer in three attempts - bailing...")
+    exit(1)
         
 
 def archive_build():
@@ -52,8 +51,8 @@ def archive_build():
 
 
 def upload():
-    green("Uploading tarball")
-    green("Uploading %(local_tar)s to %(unpack_path)s" % env)
+    print green("Uploading tarball")
+    print green("Uploading %(local_tar)s to %(unpack_path)s" % env)
     put(env.local_tar, env.unpack_path)
 
 
@@ -62,20 +61,20 @@ def unpack():
         sudo('rm -rf %(tmp_code_dir)s' % env)
     sudo("mkdir -p %(tmp_code_dir)s" % env)
 
-    green("Unpacking tarball to %s" % env.tmp_code_dir)
+    print green("Unpacking tarball to %s" % env.tmp_code_dir)
 
     with cd(env.tmp_code_dir):
         sudo('tar xvzf %(unpack_path)s' % env)
 
-    green("Doing pip install")
+    print green("Doing pip install")
     sudo('pip install -r %(tmp_code_dir)s/deploy/requirements.txt' % env)
 
-    green("Moving www dir into place")
+    print green("Moving www dir into place")
     if exists(env.code_dir):
         sudo("rm -rf %(code_dir)s" % env)
     sudo('mv %(tmp_code_dir)s/www %(code_dir)s' % env)
 
-    green("Cleaning up")
+    print green("Cleaning up")
     sudo('rm -rf %(unpack_path)s %(tmp_code_dir)s' % env)
 
 
@@ -90,15 +89,18 @@ def backup_db():
 
 
 def uwsgi():
+    print green("Updating uwsgi")
     put('deploy/uwsgi.sh', '/var/www/uwsgi.sh')
     sudo('chmod +x /var/www/uwsgi.sh')
 
 
 def supervisor():
+    print green("Updating supervisor")
     put('deploy/supervisor.conf', '/etc/supervisor/conf.d/clin.conf')
     sudo('service supervisor restart')
 
 
 def nginx():
+    print green("Updating nginx")
     put('deploy/nginx.conf', '/etc/nginx/sites-enabled/clin.conf')
     sudo('/etc/init.d/nginx restart')
